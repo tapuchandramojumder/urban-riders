fd9eff5011c053a1dabe3ba1136b72ce38748f45
tapuchandramojumder <mojumdertapu@gmaile.com>
2021-08-25 10:12:21 +0600

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import logo from './images/logoRide.png'
-
+import car from './images/Frame-1.png'
+import bike from './images/Frame-2.png'
+import bus from './images/Frame.png'
+import train from './images/Group.png'
 function App() {
   return (
     <div className="App">
@@ -12,11 +15,29 @@ function App() {
             <li><a href="destination">Destination</a></li>
             <li><a href="blog">Blog</a></li>
             <li><a href="contact">Contact</a></li>
-            <a class="login-btn" href="#"><button>Login</button></a>
+            <li><a class="login-btn" href="#">Login</a></li>
           </ul>
+         
         </nav>
-      
       </header>
+      <main  className="container">
+        <div  className="bike box">
+          <img src={bike} alt="bike" />
+          <h3>Bike</h3>
+        </div>
+        <div  className="car box">
+          <img src={car} alt="car" />
+          <h3>Car</h3>
+        </div>
+        <div  className="bus box">
+          <img src={bus} alt="bus" />
+          <h3>Bus</h3>
+        </div>
+        <div className="train box">
+          <img src={train} alt="train" />
+          <h3>Train</h3>
+        </div>
+      </main>
     </div>
   );
 }
